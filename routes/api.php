039921a34d91b8e36de96c6b8ee8ba18e5831d2e
hotza140 <yuthapichai140@gmail.com>
2025-05-08 11:21:11 +0700

    FIX

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::get('receiveconfirmmailck',[App\Http\Controllers\UserFrontendController::class,'receiveconfirmmailck'])->name('api.receiveconfirmmailck');
 
-Route::get('test_api',[App\Http\Controllers\ApirontendController::class,'test_api']);
+Route::get('test_api',[App\Http\Controllers\ApiController::class,'test_api']);
