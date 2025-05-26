2c49bce89eeeceb3f62320fe1fa6e964da3fcfd3
Orangeproject Joe <orangeprojectjoe@hotmail.com>
2025-05-26 19:40:29 +0700

    Change Mail auto01

--- resources/views/frontend/mailcus/mailtocusauto.blade.php
+++ resources/views/frontend/mailcus/mailtocusauto.blade.php
@@ -5,7 +5,8 @@
     <title>Document</title>
 </head>
 <body>
-    <p><img src="{{$message->embed($ImageLinklogo)}}" alt="" style="width: 80px;"></p>
+    {{-- <p><img src="{{$message->embed($ImageLinklogo)}}" alt="" style="width: 80px;"></p> --}}
+    <p><b><h1>NF THEATER.</h1></b></p>
     <p><h1>ยืนยันอีเมล์ กับทาง NF THEATER.</h1></p>
     <p>เมล {{$emailconfirmIS}} ของท่านที่ต้องการยืนยันกับทาง NF THEATER.</p>
     <p>{{route('api.receiveconfirmmailck')}}?rtoken={{$genTokenIS}}</p>
