909aba9142c734c2e40f0ef14964c4d84c93282f
Orangeproject Joe <orangeprojectjoe@hotmail.com>
2025-06-12 00:44:03 +0700

    clipboard netflix frontend profilev.2

--- resources/views/frontend/profile.blade.php
+++ resources/views/frontend/profile.blade.php
@@ -68,24 +68,26 @@
                             {{@$ac->password}}</h2><span class="name-profile"
                             style="color: var(--bs-emphasis-color);font-size:15px;" id="userid"></span>
 
-                            <!--copy clipboard mail pass-->
-                            <style>
-                                .icon-red {
-                                    fill: red;
-                                }
-                            </style>
-                            <!--background-color: rgb(245, 156, 156);-->
-                            <div class="edit-icon" title="Copy to Clipboard.">
-                                <button style="border:none;border-radius:50%;" onclick="this.style='border:none;border-radius:50%;background-color: rgb(245, 156, 156);'; setTimeout(() => { this.style='border:none;border-radius:50%;background-color: none;' }, 250); copyUserInfo('{{@$ac->email}}', '{{@$ac->password}}');">
-                                    <svg aria-hidden="true" focusable="false" class="icon icon-red" xmlns="http://www.w3.org/2000/svg" 
-                                            viewBox="0 0 448 512" width="1em" height="1em">
-                                            <path
-                                                d="M320 448H48c-26.5 0-48-21.5-48-48V128h48v272h272v48zm128-80V48c0-26.5-21.5-48-48-48H128c-26.5 
-                                                0-48 21.5-48 48v320c0 26.5 21.5 48 48 48h272c26.5 0 48-21.5 48-48z" />
-                                        </svg>
-                                </button>
-                            </div>
-                            <!--copy clipboard mail pass-->
+                            @if(@$ac->email)
+                                <!--copy clipboard mail pass-->
+                                <style>
+                                    .icon-red {
+                                        fill: red;
+                                    }
+                                </style>
+                                <!--background-color: rgb(245, 156, 156);-->
+                                <div class="edit-icon" title="Copy to Clipboard.">
+                                    <button style="border:none;border-radius:50%;" onclick="this.style='border:none;border-radius:50%;background-color: rgb(245, 156, 156);'; setTimeout(() => { this.style='border:none;border-radius:50%;background-color: none;' }, 250); copyUserInfo('{{@$ac->email}}', '{{@$ac->password}}');">
+                                        <svg aria-hidden="true" focusable="false" class="icon icon-red" xmlns="http://www.w3.org/2000/svg" 
+                                                viewBox="0 0 448 512" width="1em" height="1em">
+                                                <path
+                                                    d="M320 448H48c-26.5 0-48-21.5-48-48V128h48v272h272v48zm128-80V48c0-26.5-21.5-48-48-48H128c-26.5 
+                                                    0-48 21.5-48 48v320c0 26.5 21.5 48 48 48h272c26.5 0 48-21.5 48-48z" />
+                                            </svg>
+                                    </button>
+                                </div>
+                                <!--copy clipboard mail pass-->
+                            @endif
 
                         <!-- <h2 class="pass-profile">Password : {{@$ac->password}}<span id="passwordnf"></span></h2> -->
 
@@ -100,24 +102,26 @@
                             {{@$ac->password01}}</h2><span class="name-profile"
                             style="color: var(--bs-emphasis-color);font-size:15px;" id="userid"></span>
 
-                            <!--copy clipboard mail01 pass01-->
-                            <style>
-                                .icon-red {
-                                    fill: red;
-                                }
-                            </style>
-                            <!--background-color: rgb(245, 156, 156);-->
-                            <div class="edit-icon" title="Copy to Clipboard.">
-                                <button style="border:none;border-radius:50%;" onclick="this.style='border:none;border-radius:50%;background-color: rgb(245, 156, 156);'; setTimeout(() => { this.style='border:none;border-radius:50%;background-color: none;' }, 250); copyUserInfo('{{@$ac->email01}}', '{{@$ac->password01}}');">
-                                    <svg aria-hidden="true" focusable="false" class="icon icon-red" xmlns="http://www.w3.org/2000/svg" 
-                                            viewBox="0 0 448 512" width="1em" height="1em">
-                                            <path
-                                                d="M320 448H48c-26.5 0-48-21.5-48-48V128h48v272h272v48zm128-80V48c0-26.5-21.5-48-48-48H128c-26.5 
-                                                0-48 21.5-48 48v320c0 26.5 21.5 48 48 48h272c26.5 0 48-21.5 48-48z" />
-                                        </svg>
-                                </button>
-                            </div>
-                            <!--copy clipboard mail01 pass01-->
+                            @if(@$ac->email01)
+                                <!--copy clipboard mail01 pass01-->
+                                <style>
+                                    .icon-red {
+                                        fill: red;
+                                    }
+                                </style>
+                                <!--background-color: rgb(245, 156, 156);-->
+                                <div class="edit-icon" title="Copy to Clipboard.">
+                                    <button style="border:none;border-radius:50%;" onclick="this.style='border:none;border-radius:50%;background-color: rgb(245, 156, 156);'; setTimeout(() => { this.style='border:none;border-radius:50%;background-color: none;' }, 250); copyUserInfo('{{@$ac->email01}}', '{{@$ac->password01}}');">
+                                        <svg aria-hidden="true" focusable="false" class="icon icon-red" xmlns="http://www.w3.org/2000/svg" 
+                                                viewBox="0 0 448 512" width="1em" height="1em">
+                                                <path
+                                                    d="M320 448H48c-26.5 0-48-21.5-48-48V128h48v272h272v48zm128-80V48c0-26.5-21.5-48-48-48H128c-26.5 
+                                                    0-48 21.5-48 48v320c0 26.5 21.5 48 48 48h272c26.5 0 48-21.5 48-48z" />
+                                            </svg>
+                                    </button>
+                                </div>
+                                <!--copy clipboard mail01 pass01-->
+                            @endif
 
                         <!-- <h2 class="pass-profile">Password : {{@$ac->password01}}<span id="passwordnf"></span></h2> -->
                         @else
@@ -129,24 +133,26 @@
                             {{@$ac->password02}}</h2><span class="name-profile"
                             style="color: var(--bs-emphasis-color);font-size:15px;" id="userid"></span>
 
-                            <!--copy clipboard mail02 pass02-->
-                            <style>
-                                .icon-red {
-                                    fill: red;
-                                }
-                            </style>
-                            <!--background-color: rgb(245, 156, 156);-->
-                            <div class="edit-icon" title="Copy to Clipboard.">
-                                <button style="border:none;border-radius:50%;" onclick="this.style='border:none;border-radius:50%;background-color: rgb(245, 156, 156);'; setTimeout(() => { this.style='border:none;border-radius:50%;background-color: none;' }, 250); copyUserInfo('{{@$ac->email02}}', '{{@$ac->password02}}');">
-                                    <svg aria-hidden="true" focusable="false" class="icon icon-red" xmlns="http://www.w3.org/2000/svg" 
-                                            viewBox="0 0 448 512" width="1em" height="1em">
-                                            <path
-                                                d="M320 448H48c-26.5 0-48-21.5-48-48V128h48v272h272v48zm128-80V48c0-26.5-21.5-48-48-48H128c-26.5 
-                                                0-48 21.5-48 48v320c0 26.5 21.5 48 48 48h272c26.5 0 48-21.5 48-48z" />
-                                        </svg>
-                                </button>
-                            </div>
-                            <!--copy clipboard mail02 pass02-->
+                            @if(@$ac->email02)
+                                <!--copy clipboard mail02 pass02-->
+                                <style>
+                                    .icon-red {
+                                        fill: red;
+                                    }
+                                </style>
+                                <!--background-color: rgb(245, 156, 156);-->
+                                <div class="edit-icon" title="Copy to Clipboard.">
+                                    <button style="border:none;border-radius:50%;" onclick="this.style='border:none;border-radius:50%;background-color: rgb(245, 156, 156);'; setTimeout(() => { this.style='border:none;border-radius:50%;background-color: none;' }, 250); copyUserInfo('{{@$ac->email02}}', '{{@$ac->password02}}');">
+                                        <svg aria-hidden="true" focusable="false" class="icon icon-red" xmlns="http://www.w3.org/2000/svg" 
+                                                viewBox="0 0 448 512" width="1em" height="1em">
+                                                <path
+                                                    d="M320 448H48c-26.5 0-48-21.5-48-48V128h48v272h272v48zm128-80V48c0-26.5-21.5-48-48-48H128c-26.5 
+                                                    0-48 21.5-48 48v320c0 26.5 21.5 48 48 48h272c26.5 0 48-21.5 48-48z" />
+                                            </svg>
+                                    </button>
+                                </div>
+                                <!--copy clipboard mail02 pass02-->
+                            @endif
 
                         <!-- <h2 class="pass-profile">Password : {{@$ac->password02}}<span id="passwordnf"></span></h2> -->
                         @endif
